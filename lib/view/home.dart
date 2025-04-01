88c0ba34768811daea3d98bea212a2df175b09b9
SunnyKMhatre <sunnykmhatre333@gmail.com>
2025-04-01 17:41:22 +0530

    Added initial project files

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -47,149 +47,219 @@ class _True_FalseState extends State<True_False> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: Drawer(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      "assets/settings button@3x.png",
-                      height: 50,
-                      width: 50,
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text(
-                      "Settings",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Image.asset("assets/text-names.png"),
-              )
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.amber,
-          title: Text(
-            "True or False",
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-          ),
-          actions: [
+      drawer: Drawer(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset("assets/share icon.png"),
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/settings button@3x.png",
+                    height: 50,
+                    width: 50,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "Settings",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Image.asset("assets/text-names.png"),
             )
           ],
         ),
-        body: Center(
-          child: Stack(
-            children: [
-              details == null
-                  ? Center(child: CircularProgressIndicator())
-                  : Image.asset(
-                      "assets/bg.png",
-                      fit: BoxFit.fill,
-                      height: double.infinity,
-                      width: double.infinity,
-                    ),
-              Positioned(
-                  left: 120,
-                  top: 90,
-                  child: Image.asset("assets/true false icon.png")),
-              Positioned(
-                  left: MediaQuery.of(context).size.width *
-                      0.1, 
-                  right: MediaQuery.of(context).size.width *
-                      0.1, 
-                  top: 280, 
-                  bottom: MediaQuery.of(context).size.height *
-                      0.2, 
-                  child: Center(
-                    child: details == null
-                        ? Center(child: CircularProgressIndicator())
-                        : GridView(
-                            physics: NeverScrollableScrollPhysics(),
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2),
-                            children: [
-                              for (int i = 0; i < details!.length; i++) ...[
-                                // Image.network(details?[i].question ?? "NA")
-                                details!.isEmpty
-                                    ? Text("No Data")
-                                    : GestureDetector(
-                                        onTap: () {
-                                          if (i == 0) {
-                                            // Navigator.push(MaterialPageRoute)
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        Person()));
-                                          } else if (i == 1) {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        Geography()));
-                                          } else if (i == 2) {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        Animals()));
-                                          } else if (i == 3) {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        Plants()));
-                                          }
-                                        },
-                                        child: Column(
-                                          children: [
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.all(8.0),
-                                              child: Image.network(
-                                                  details?[i].catImage ?? "NA"),
-                                            )
-                                          ],
-                                        ),
-                                      )
-                              ]
-                            ],
-                          ),
-                  )),
-              Positioned(
-                  left: MediaQuery.of(context).size.width *
-                      0.1, // 10% from the left
-                  right: MediaQuery.of(context).size.width *
-                      0.1, // 10% from the right
-                  // top: MediaQuery.of(context).size.height *
-                  //     0.2, // 20% from the top
-                  bottom: MediaQuery.of(context).size.height *
-                      0, // 20% from the bottom
-                  child: Image.asset(
-                    "assets/footer.png",
-                    // fit: BoxFit.fill,
-                    // height: double.infinity,
-                    // width: double.infinity,
-                  ))
-            ],
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        title: Text(
+          "True or False",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("assets/share icon.png"),
+          )
+        ],
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/bg.png"),
+            fit: BoxFit.fill,
           ),
-        ));
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset("assets/true false icon.png"),
+            ),
+            Expanded(
+              child: details == null
+                  ? Center(child: CircularProgressIndicator())
+                  : GridView.builder(
+                      padding: const EdgeInsets.all(10),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8,
+                        childAspectRatio: 1,
+                      ),
+                      itemCount: details!.length,
+                      itemBuilder: (context, i) {
+                        return GestureDetector(
+                            onTap: () {
+                              if (i == 0) {
+                                // Navigator.push(MaterialPageRoute)
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Person()));
+                              } else if (i == 1) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Geography()));
+                              } else if (i == 2) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Animals()));
+                              } else if (i == 3) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Plants()));
+                              }
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.yellow, width: 10),
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              child: Image.network(
+                                details![i].catImage ?? "",
+                                // fit: BoxFit.cover,
+                              ),
+                            ));
+                      },
+                    ),
+            ),
+            Image.asset(
+              "assets/footer.png",
+            )
+          ],
+        ),
+      ),
+      // body: Center(
+      //   child: Stack(
+      //     children: [
+      //       details == null
+      //           ? Center(child: CircularProgressIndicator())
+      //           : Image.asset(
+      //               "assets/bg.png",
+      //               fit: BoxFit.fill,
+      //               height: double.infinity,
+      //               width: double.infinity,
+      //             ),
+      //       Positioned(
+      //           left: 120,
+      //           top: 90,
+      //           child: Image.asset("assets/true false icon.png")),
+      //       Positioned(
+      //           left: MediaQuery.of(context).size.width * 0.1,
+      //           right: MediaQuery.of(context).size.width * 0.1,
+      //           top: 280,
+      //           bottom: MediaQuery.of(context).size.height * 0.2,
+      //           child: Center(
+      //             child: details == null
+      //                 ? Center(child: CircularProgressIndicator())
+      //                 : GridView(
+      //                     physics: NeverScrollableScrollPhysics(),
+      //                     gridDelegate:
+      //                         SliverGridDelegateWithFixedCrossAxisCount(
+      //                             crossAxisCount: 2, mainAxisExtent: 100),
+      //                     children: [
+      //                       for (int i = 0; i < details!.length; i++) ...[
+      //                         // Image.network(details?[i].question ?? "NA")
+      //                         details!.isEmpty
+      //                             ? Text("No Data")
+      //                             : GestureDetector(
+      //                                 onTap: () {
+      //                                   if (i == 0) {
+      //                                     // Navigator.push(MaterialPageRoute)
+      //                                     Navigator.push(
+      //                                         context,
+      //                                         MaterialPageRoute(
+      //                                             builder: (context) =>
+      //                                                 Person()));
+      //                                   } else if (i == 1) {
+      //                                     Navigator.push(
+      //                                         context,
+      //                                         MaterialPageRoute(
+      //                                             builder: (context) =>
+      //                                                 Geography()));
+      //                                   } else if (i == 2) {
+      //                                     Navigator.push(
+      //                                         context,
+      //                                         MaterialPageRoute(
+      //                                             builder: (context) =>
+      //                                                 Animals()));
+      //                                   } else if (i == 3) {
+      //                                     Navigator.push(
+      //                                         context,
+      //                                         MaterialPageRoute(
+      //                                             builder: (context) =>
+      //                                                 Plants()));
+      //                                   }
+      //                                 },
+      //                                 child: Column(
+      //                                   children: [
+      //                                     Image.network(
+      //                                       details?[i].catImage ?? "NA",
+      //                                       fit: BoxFit.contain,
+      //                                     )
+      //                                   ],
+      //                                 ),
+      //                               )
+      //                       ]
+      //                     ],
+      //                   ),
+      //           )),
+      //       Positioned(
+      //           left: MediaQuery.of(context).size.width *
+      //               0.1, // 10% from the left
+      //           right: MediaQuery.of(context).size.width *
+      //               0.1, // 10% from the right
+      //           // top: MediaQuery.of(context).size.height *
+      //           //     0.2, // 20% from the top
+      //           bottom: MediaQuery.of(context).size.height *
+      //               0, // 20% from the bottom
+      //           child: Image.asset(
+      //             "assets/footer.png",
+      //             // fit: BoxFit.fill,
+      //             // height: double.infinity,
+      //             // width: double.infinity,
+      //           ))
+      //     ],
+      //   ),
+      // )
+    );
   }
 }
